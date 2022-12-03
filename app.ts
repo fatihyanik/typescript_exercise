117cf03443238adbd325eb358c27a61d12311fad
fatihyanik <fatihyanik07@gmail.com>
2022-12-03 12:53:40 +0100

    added array types

--- app.ts
+++ app.ts
@@ -5,6 +5,16 @@ const person: {
     //const person = {
     name:"Fatih",
     age: 29,
+    hobbies: ['Sports', 'Cooking']
 }
 
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for(const hobby of person.hobbies){
+    console.log(hobby.toUpperCase());
+    //console.log(hobby.map()); // !!! ERROR !!!
+    
+}
