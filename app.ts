26bb962af334e46fc2806759ec5a28c77a7e573e
fatihyanik <fatihyanik07@gmail.com>
2022-12-03 13:17:51 +0100

    added enums

--- app.ts
+++ app.ts
@@ -2,18 +2,30 @@
 //     name:string;
 //     age:number;
 // } = {
-    const person: {
-        name: string;
-        age: number;
-        hobbies: string[];
-        role: [number, string]
-    } = {
-    name:"Fatih",
+//     const person: {
+//         name: string;
+//         age: number;
+//         hobbies: string[];
+//         role: [number, string]
+//     } = {
+//     name:"Fatih",
+//     age: 29,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// };
+
+//const ADMIN = 0;
+//const READ_ONLY = 1;
+//const AUTHOR = 2;
+
+enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR' }
+
+const person = {
+    name: 'Fatih',
     age: 29,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
-
 //person.role.push('admin');
 //person.role[1] = 10;
 
@@ -24,7 +36,12 @@ favoriteActivities = ['Sports'];
 
 console.log(person.name);
 
-for(const hobby of person.hobbies){
+for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
     //console.log(hobby.map()); // !!! ERROR !!!
+}
+
+if (person.role === Role.AUTHOR) {
+    console.log('is author');
+
 }
