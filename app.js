07153688840f85876fc837f93b457b0189104f37
fatihyanik <fatihyanik07@gmail.com>
2022-12-03 13:00:21 +0100

    added tuple types

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+// const person: {
+//     name:string;
+//     age:number;
+// } = {
 var person = {
-    //const person = {
     name: "Fatih",
     age: 29,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
+person.role.push('admin');
+//person.role[1] = 10;
 var favoriteActivities;
 favoriteActivities = ['Sports'];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+    //console.log(hobby.map()); // !!! ERROR !!!
 }
--- app.ts
+++ app.ts
@@ -1,12 +1,23 @@
-const person: {
-    name:string;
-    age:number;
-} = {
-    //const person = {
+// const person: {
+//     name:string;
+//     age:number;
+// } = {
+    const person: {
+        name: string;
+        age: number;
+        hobbies: string[];
+        role: [number, string]
+    } = {
     name:"Fatih",
     age: 29,
-    hobbies: ['Sports', 'Cooking']
-}
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
+};
+
+//person.role.push('admin');
+//person.role[1] = 10;
+
+//person.role = [0, 'admin', 'user']
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
@@ -16,5 +27,4 @@ console.log(person.name);
 for(const hobby of person.hobbies){
     console.log(hobby.toUpperCase());
     //console.log(hobby.map()); // !!! ERROR !!!
-    
 }
