62a20611f814412f50aaf98065b8d66098ab3ccf
fatihyanik <fatihyanik07@gmail.com>
2022-12-04 20:05:41 +0100

    added tsconfig.json

--- app.ts
+++ app.ts
@@ -1,16 +1,3 @@
-let userInput :unknown;
-let userName: string;
+const userName = 'Murat Fatih';
 
-userInput = 5;
-userInput ="Max";
-if(typeof userInput === 'string'){
-    userName = userInput;
-}
-
-function generateError (message: string, code:number): never{
-    throw{message:message, errorCode: code};
-    // while (true){}
-}
-
-generateError('An error occured', 500 )
-
+console.log(userName);
