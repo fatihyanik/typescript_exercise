de45b31066caec2464da9a4ad2632860d47ef947
fatihyanik <fatihyanik07@gmail.com>
2022-12-04 19:41:26 +0100

    added unknown types

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ function printResult(num) {
     console.log('Result: ' + num);
     return;
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 printResult(add(5, 12));
 var combineValues;
 combineValues = add;
 console.log(combineValues(8, 8));
 //let someValue: undefined;
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
