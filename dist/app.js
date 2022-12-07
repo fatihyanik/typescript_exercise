3fb6661b7dbd2b6b8b253527217ca54aca1c2c07
fatihyanik <fatihyanik07@gmail.com>
2022-12-07 22:15:27 +0100

    added this keyword

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,13 @@
 "use strict";
-// Code goes here!
+class Department {
+    constructor(n) {
+        this.name = n;
+    }
+    describe() {
+        console.log('Department: ' + this.name);
+    }
+}
+const accounting = new Department('Accounting');
+accounting.describe();
+const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+accountingCopy.describe();
