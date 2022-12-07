f78fca7236082e2146a986fa4c4312ff7e3589ef
fatihyanik <fatihyanik07@gmail.com>
2022-12-07 22:16:44 +0100

    added private and public

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,27 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
     }
     describe() {
         console.log('Department: ' + this.name);
     }
+    addEmployee(employee) {
+        // validation etc
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 const accounting = new Department('Accounting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+// accounting.employees[2] = 'Anna';
 accounting.describe();
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-accountingCopy.describe();
+accounting.name = 'NEW NAME';
+accounting.printEmployeeInformation();
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+// accountingCopy.describe();
